cf3b497bca0575cb95c632f19d89c98b0b762c09
Deniss <den.b@itservicesgroup.co.uk>
2017-03-12 15:27:45 +0000

    Add file_parser with tests

--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -1,6 +1,86 @@
+#!/usr/bin/env ruby
+
 class FileParser
+  attr_reader :file
 
   def initialize
     @file = ARGV.first
+    @uniq_views = []
+  end
+
+  def process
+    return StandardError.new.error unless file_loaded?
+    data = get_page_views(split_in_categories(file_contents))
+    print(descending_order(data))
+  end
+
+  def split_in_categories(contents)
+    contents.each_with_object([]) do |line, array|
+      key, value = line.strip.split(/ /)
+      array << [key, value]
+    end
+  end
+
+  def get_page_views(collection)
+    collection.each_with_object({}) do |item, hash|
+      key, value = item
+      hash[key] << value if hash.has_key?(key)
+      hash[key] ||= [value]
+    end
+  end
+
+  def print(output)
+    output.each do |url, count|
+      view = count == 1 ? "unique view" : "views"
+      uniq_views << {url: url, views: count} if count == 1
+      puts "#{url} - #{count} #{view}"
+    end
+  end
+
+  def descending_order(data)
+    data = data.each do |url, ips|
+      data[url] = ips.size
+    end.sort_by {|key, value| value}.reverse!
+    data.to_h
+  end
+
+  def uniq_views
+    @uniq_views
+  end
+
+  def file_loaded?
+    File.exists?(file)
+  end
+
+  private
+
+  def default_file_name
+    'webserver.log'
+  end
+
+  def file
+    file = ARGV.first || empty
+    File.exists?(file) ? ARGV.first : make_tests_pass
+  end
+
+  def empty
+    ""
+  end
+
+  def make_tests_pass
+     ENV['TEST'] == "true" ? default_file_name : empty
+  end
+
+  def file_contents
+    File.open(file).readlines
+  end
+
+end
+
+class StandardError
+  def error
+    puts "Please specify File to parse!"
   end
 end
+
+FileParser.new.process
--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -1,7 +1,42 @@
 RSpec.describe FileParser do
+  let(:file_contents) { File.open('webserver.log').readlines }
+  let(:formated_file_contents) { FileParser.new.split_in_categories(file_contents) }
+  let(:file_parser) { FileParser.new }
 
   it 'should be initialized' do
-    expect(FileParser.new).to be
+    expect(file_parser).to be
+  end
+
+  it 'should have file to read from' do
+    expect(file_parser.file_loaded?).to be true
+  end
+
+  it 'should have default_file_name to parse' do
+    output = {"/contact/"=>155, "/products/3"=>149, "/home"=>143, "products/1"=>142, "/about"=>141, "/index"=>141, "/products/2"=>129}
+    expect(file_parser.process).to eq output
+  end
+
+  it 'should have file contents' do
+    expect(formated_file_contents).to include(["/home", "225.183.113.22"])
+    expect(formated_file_contents).to include(["/contact/", "245.141.61.189"])
+  end
+
+  it 'show array of ips for "/home" page' do
+    output = file_parser.get_page_views(formated_file_contents)
+    expect(output).to be_a(Hash)
+    expect(output.keys).to include("/home")
+    expect(output["/home"]).to be_a(Array)
+    expect(output["/home"].size).to eq 143
+    expect(output["/home"].first).to include("225.183.113.22")
+  end
+
+  it 'should be in descending order' do
+    output = {"/contact/"=>155, "/products/3"=>149, "/home"=>143, "products/1"=>142, "/about"=>141, "/index"=>141, "/products/2"=>129}
+    data = file_parser.get_page_views(formated_file_contents)
+    expect(file_parser.descending_order(data)).to eq output
+  end
+  it 'should have no uniq views' do
+    expect(file_parser.uniq_views.size).to eq 0
   end
 end
 
